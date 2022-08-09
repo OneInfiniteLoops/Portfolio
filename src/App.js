3114645f0dfd25e4575bfbc9c8add19ba90af074
1-Infiniteloop <skjyeung@gmail.com>
2022-08-09 14:42:31 +0100

    imports and renders Header component at app level

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
-function App() {}
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
 
 export default App;
