cf08f7d9548eab78187f3a5a1150dadbaba08963
1-Infiniteloop <skjyeung@gmail.com>
2022-08-09 17:12:57 +0100

    Adds Introduction component

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,15 @@
+import Profile from "./Profile";
+
+const Introduction = () => {
+  return (
+    <div className="Introduction">
+      <p>Hi, I am</p>
+      <h1>Siu Kwan James Yeung.</h1>
+      <h2>Full-Stack Software Developer.</h2>
+      <h3>A highly motivated and curious software developer.</h3>
+      <Profile />
+    </div>
+  );
+};
+
+export default Introduction;
