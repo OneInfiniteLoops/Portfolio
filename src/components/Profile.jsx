31c44f3a435b2ac3c7d948b25a5dd8467e1700ce
1-Infiniteloop <skjyeung@gmail.com>
2022-08-09 17:13:05 +0100

    Adds Profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import profile from "../images/profile.jpg";
+
+const ProfileImage = () => {
+  return (
+    <div className="ProfileImage">
+      <img src={profile} alt="profile" />
+    </div>
+  );
+};
+
+export default ProfileImage;
