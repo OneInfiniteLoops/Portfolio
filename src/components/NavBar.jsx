a9b98b3ab4c8ba7f2b82c4ba97871c1c6105bae3
1-Infiniteloop <skjyeung@gmail.com>
2022-08-10 13:44:29 +0100

    Adds links to NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
     <div className="NavBar">
       <h1>/james.</h1>
+      <nav className="NavLinks">
+        <Link to="/#skills" className="EachNavBarLink">
+          SKILLS & EXPERIENCE
+        </Link>
+        <Link to="/#work" className="EachNavBarLink">
+          WORK
+        </Link>
+        <Link to="/#contact" className="EachNavBarLink">
+          CONTACT
+        </Link>
+      </nav>
     </div>
   );
 };
