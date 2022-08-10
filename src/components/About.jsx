8e69360057eee32292b538d32f561678e3bec9cc
1-Infiniteloop <skjyeung@gmail.com>
2022-08-10 14:37:20 +0100

    creates About component

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div id="About">
+      <h1>About</h1>
+      <p>
+        As a long-time tech enthusiast who loves learning about the latest
+        innovations, I recently embarked on my journey to become a full-stack
+        developer through the Northcoders’ coding bootcamp in Manchester, UK. At
+        the bootcamp, I gained hands-on experience in developing web and mobile
+        applications, various key skills and industry best practices such as
+        Test-Driven Development (TDD). I also volunteered to present a
+        live-streamed lightning talk which has reinforced my passion for
+        technology – and its impact on society.
+      </p>
+    </div>
+  );
+};
+
+export default About;
