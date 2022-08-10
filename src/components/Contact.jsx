63b5bdc2390afc139e8f10bf895f12a3fe9b6f2a
1-Infiniteloop <skjyeung@gmail.com>
2022-08-10 18:20:16 +0100

    Imports and links social media icons for contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,26 @@
+import email from "../images/email_icon.png";
+import linkedin from "../images/linkedin_icon.png";
+import github from "../images/github_icon.png";
+
 const Contact = () => {
-  return <div id="contact"></div>;
+  return (
+    <div id="contact">
+      <h1>Get In Touch.</h1>
+      <p>
+        Please feel free to drop me an email. Alternatively, you can find me on
+        LinkedIn and GitHub.
+      </p>
+      <a href="mailto:skjyeung@gmail.com">
+        <img className="Social-Icons" src={email} alt="email_icon"></img>
+      </a>
+      <a href="https://www.linkedin.com/in/jamesyeung1/">
+        <img className="Social-Icons" src={linkedin} alt="linkedin_icon"></img>
+      </a>
+      <a href="https://github.com/OneInfiniteLoops">
+        <img className="Social-Icons" src={github} alt="github_icon"></img>
+      </a>
+    </div>
+  );
 };
 
 export default Contact;
