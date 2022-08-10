e71bf3ff05dd157e17528f74669a10fd3decd99c
1-Infiniteloop <skjyeung@gmail.com>
2022-08-10 16:19:38 +0100

    Amend import to react-router-hash-link and add smooth scroll

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 
 const NavBar = () => {
   return (
     <div className="NavBar">
       <h1>/james.</h1>
       <nav className="NavLinks">
-        <Link to="/#about" className="EachNavBarLink">
+        <Link to="#about" smooth className="EachNavBarLink">
           ABOUT
         </Link>
-        <Link to="/#work" className="EachNavBarLink">
+        <Link to="#work" smooth className="EachNavBarLink">
           WORK
         </Link>
-        <Link to="/#contact" className="EachNavBarLink">
+        <Link to="#contact" smooth className="EachNavBarLink">
           CONTACT
         </Link>
       </nav>
