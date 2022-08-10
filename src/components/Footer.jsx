49fa61eedb7cff77e436d7e14915c35caf66df53
1-Infiniteloop <skjyeung@gmail.com>
2022-08-10 13:10:36 +0100

    Creates Footer component for export

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p>Designed & Built by Siu Kwan James Yeung.</p>
+      <p> &copy; 2022 Siu Kwan James Yeung. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
