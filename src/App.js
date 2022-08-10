62ab0acceaf96f370dbacdf1e831841ac40f77b7
1-Infiniteloop <skjyeung@gmail.com>
2022-08-10 13:10:47 +0100

    Adds Footer Component to return

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Introduction from "./components/Introduction";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Introduction />
+      <Footer />
     </div>
   );
 }
