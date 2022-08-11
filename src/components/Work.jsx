263c71b2ab7ea6910cc82263c26719d88ac19a4f
1-Infiniteloop <skjyeung@gmail.com>
2022-08-11 22:58:43 +0100

    Populates description value for all portfolioCards

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,7 +6,8 @@ const Work = () => {
     {
       name: "News-API",
       type: "Back-End",
-      description: "This is a portfolio card",
+      description:
+        "The REST API is built with Node.js (JavaScript) and the Express framework. It is a back-end web application that contains a selection of endpoints where users can interact with the PostgreSQL database. Features include enabling users to access stored articles, post comments, and update vote count.",
       img_url: "https://via.placeholder.com/150",
       liveDemoLink: "https://news-space.herokuapp.com/",
       githubLink: "https://github.com/OneInfiniteLoops/News-API",
@@ -14,7 +15,8 @@ const Work = () => {
     {
       name: "NewsSpace",
       type: "Front-End",
-      description: "This is a portfolio card",
+      description:
+        "A Reddit-like social news aggregation app. It is a mobile-first front-end application built with React.js (JavaScript), HTML5 and CSS3. It provides users with the ability to interact with the back-end News API via a user-accessible interface.",
       img_url: "https://via.placeholder.com/150",
       liveDemoLink: "https://newsspace-project.netlify.app/",
       githubLink: "https://github.com/OneInfiniteLoops/newsspace",
@@ -22,7 +24,8 @@ const Work = () => {
     {
       name: "StudyOut",
       type: "Mobile App",
-      description: "This is a portfolio card",
+      description:
+        "Designed to help students find public study spaces more easily, the crowd-sourced application shows users nearby study spaces with key information such as specific features and suggested conditions of use. The mobile application was developed using React Native and Expo for the front-end, and Go/Golang and Fiber for the back-end. The area search feature and its map interface are powered by Google Places API and Google Maps respectively.",
       img_url: "https://via.placeholder.com/150",
       liveDemoLink: "To Be Updated",
       githubLink: "https://github.com/OneInfiniteLoops/studyout",
