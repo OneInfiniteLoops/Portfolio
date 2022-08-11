bb6d9ab4cb15780dd8df93107ea97fd6e6e365d1
1-Infiniteloop <skjyeung@gmail.com>
2022-08-12 00:05:50 +0100

    Imports/renders screenshots, adds img styling

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -4,7 +4,11 @@ import link_icon from "../images/link_icon.png";
 const PortfolioCard = ({ portfolioItem }) => {
   return (
     <li className="portfolio-card">
-      <img src={portfolioItem.img_url} alt={portfolioItem.name} />
+      <img
+        className="project-screenshot"
+        src={portfolioItem.img_url}
+        alt={portfolioItem.name}
+      />
       <h1>{portfolioItem.name}</h1>
       <h2>{portfolioItem.type}</h2>
       <p>{portfolioItem.description}</p>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import PortfolioCard from "./PortfolioCard";
+import newsapi_screenshot from "../images/newsapi_screenshot.png";
+import newsspace_screeen from "../images/newsspace_screenshot.jpg";
+import studyout_screenshot from "../images/studyout_screenshot.PNG";
 
 const Work = () => {
   const [portfolioList, setPortfolioList] = useState([
@@ -8,7 +11,7 @@ const Work = () => {
       type: "Back-End",
       description:
         "The REST API is built with Node.js (JavaScript) and the Express framework. It is a back-end web application that contains a selection of endpoints where users can interact with the PostgreSQL database. Features include enabling users to access stored articles, post comments, and update vote count.",
-      img_url: "https://via.placeholder.com/150",
+      img_url: newsapi_screenshot,
       liveDemoLink: "https://news-space.herokuapp.com/",
       githubLink: "https://github.com/OneInfiniteLoops/News-API",
     },
@@ -17,7 +20,7 @@ const Work = () => {
       type: "Front-End",
       description:
         "A Reddit-like social news aggregation app. It is a mobile-first front-end application built with React.js (JavaScript), HTML5 and CSS3. It provides users with the ability to interact with the back-end News API via a user-accessible interface.",
-      img_url: "https://via.placeholder.com/150",
+      img_url: newsspace_screeen,
       liveDemoLink: "https://newsspace-project.netlify.app/",
       githubLink: "https://github.com/OneInfiniteLoops/newsspace",
     },
@@ -26,7 +29,7 @@ const Work = () => {
       type: "Mobile App",
       description:
         "Designed to help students find public study spaces more easily, the crowd-sourced application shows users nearby study spaces with key information such as specific features and suggested conditions of use. The mobile application was developed using React Native and Expo for the front-end, and Go/Golang and Fiber for the back-end. The area search feature and its map interface are powered by Google Places API and Google Maps respectively.",
-      img_url: "https://via.placeholder.com/150",
+      img_url: studyout_screenshot,
       liveDemoLink: "To Be Updated",
       githubLink: "https://github.com/OneInfiniteLoops/studyout",
     },
