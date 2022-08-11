787ea968a16c572766b4dd26307d145a37fd6447
1-Infiniteloop <skjyeung@gmail.com>
2022-08-11 23:52:37 +0100

    Renames import

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,4 +1,4 @@
-import github from "../images/github_icon.png";
+import github_icon from "../images/github_icon.png";
 import link_icon from "../images/link_icon.png";
 
 const PortfolioCard = ({ portfolioItem }) => {
@@ -12,7 +12,7 @@ const PortfolioCard = ({ portfolioItem }) => {
         <img className="Social-Icons" src={link_icon} alt="link_icon"></img>
       </a>
       <a href={portfolioItem.githubLink}>
-        <img className="Social-Icons" src={github} alt="github_icon"></img>
+        <img className="Social-Icons" src={github_icon} alt="github_icon"></img>
       </a>
     </li>
   );
