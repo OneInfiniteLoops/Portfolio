01b861ef30458460ca989e70232617c435ecb00b
1-Infiniteloop <skjyeung@gmail.com>
2022-08-11 11:44:10 +0100

    Populate and render PortfolioCard, with useState

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import PortfolioCard from "./PortfolioCard";
+
 const Work = () => {
-  return <div id="work"></div>;
+  const [portfolioList, setPortfolioList] = useState([
+    {
+      name: "Portfolio Card1",
+      type: "Back-End",
+      description: "This is a portfolio card",
+      img_url: "https://via.placeholder.com/150",
+      liveDemoLink: "https://www.google.com",
+      githubLink: "https://www.google.com",
+    },
+    {
+      name: "Portfolio Card2",
+      type: "Front-End",
+      description: "This is a portfolio card",
+      img_url: "https://via.placeholder.com/150",
+      liveDemoLink: "https://www.google.com",
+      githubLink: "https://www.google.com",
+    },
+    {
+      name: "Portfolio Card3",
+      type: "Mobile",
+      description: "This is a portfolio card",
+      img_url: "https://via.placeholder.com/150",
+      liveDemoLink: "https://www.google.com",
+      githubLink: "https://www.google.com",
+    },
+  ]);
+
+  return (
+    <div id="work">
+      <h1>Check out my projects</h1>
+      <ul className="portfolio-list">
+        {portfolioList.map((portfolioItem) => {
+          return (
+            <PortfolioCard
+              key={portfolioItem.name}
+              portfolioItem={portfolioItem}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Work;
