08f76f57829631527505df50dea3c3bd784a85e2
1-Infiniteloop <skjyeung@gmail.com>
2022-08-12 02:55:52 +0100

    Rearranges order of key-value for data objs

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -9,29 +9,29 @@ const Work = () => {
     {
       name: "News-API",
       type: "Back-End",
+      img_url: newsapi_screenshot,
+      githubLink1: "https://github.com/OneInfiniteLoops/News-API",
+      liveDemoLink: "https://news-space.herokuapp.com/",
       description:
         "The REST API is built with Node.js (JavaScript) and the Express framework. It is a back-end web application that contains a selection of endpoints where users can interact with the PostgreSQL database. Features include enabling users to access stored articles, post comments, and update vote count.",
-      img_url: newsapi_screenshot,
-      liveDemoLink: "https://news-space.herokuapp.com/",
-      githubLink1: "https://github.com/OneInfiniteLoops/News-API",
     },
     {
       name: "NewsSpace",
       type: "Front-End",
+      img_url: newsspace_screeen,
+      githubLink1: "https://github.com/OneInfiniteLoops/newsspace",
+      liveDemoLink: "https://newsspace-project.netlify.app/",
       description:
         "A Reddit-like social news aggregation app. It is a mobile-first front-end application built with React.js (JavaScript), HTML5 and CSS3. It provides users with the ability to interact with the back-end News API via a user-accessible interface.",
-      img_url: newsspace_screeen,
-      liveDemoLink: "https://newsspace-project.netlify.app/",
-      githubLink1: "https://github.com/OneInfiniteLoops/newsspace",
     },
     {
       name: "StudyOut",
       type: "Mobile App",
-      description:
-        "Designed to help students find public study spaces more easily, the crowd-sourced application shows users nearby study spaces with key information such as specific features and suggested conditions of use. The mobile application was developed using React Native and Expo for the front-end, and Go/Golang and Fiber for the back-end. The area search feature and its map interface are powered by Google Places API and Google Maps respectively.",
       img_url: studyout_screenshot,
       githubLink1: "https://github.com/OneInfiniteLoops/studyout",
       githubLink2: "https://github.com/jaykeHarrison/studyout-api",
+      description:
+        "Designed to help students find public study spaces more easily, the crowd-sourced application shows users nearby study spaces with key information such as specific features and suggested conditions of use. The mobile application was developed using React Native and Expo for the front-end, and Go/Golang and Fiber for the back-end. The area search feature and its map interface are powered by Google Places API and Google Maps respectively.",
     },
   ]);
 
