db4321b0e1a822b2f0a8d49fb39abc19d13caa7b
1-Infiniteloop <skjyeung@gmail.com>
2022-08-17 15:40:22 +0100

    Adds extra div

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,19 +4,25 @@ import github from "../images/github_icon.png";
 
 const Contact = () => {
   return (
-    <div id="contact">
-      <h1>Get In Touch.</h1>
-      <p>Feel free to drop me an email at skjyeung@gmail.com.</p>
-      <p>You can also find me on LinkedIn and GitHub.</p>
-      <a href="mailto:skjyeung@gmail.com">
-        <img className="Social-Icons" src={email} alt="email_icon"></img>
-      </a>
-      <a href="https://www.linkedin.com/in/jamesyeung1/">
-        <img className="Social-Icons" src={linkedin} alt="linkedin_icon"></img>
-      </a>
-      <a href="https://github.com/OneInfiniteLoops">
-        <img className="Social-Icons" src={github} alt="github_icon"></img>
-      </a>
+    <div>
+      <div id="contact">
+        <h1>Get In Touch.</h1>
+        <p>Feel free to drop me an email at skjyeung@gmail.com.</p>
+        <p>You can also find me on LinkedIn and GitHub.</p>
+        <a href="mailto:skjyeung@gmail.com">
+          <img className="Social-Icons" src={email} alt="email_icon"></img>
+        </a>
+        <a href="https://www.linkedin.com/in/jamesyeung1/">
+          <img
+            className="Social-Icons"
+            src={linkedin}
+            alt="linkedin_icon"
+          ></img>
+        </a>
+        <a href="https://github.com/OneInfiniteLoops">
+          <img className="Social-Icons" src={github} alt="github_icon"></img>
+        </a>
+      </div>
     </div>
   );
 };
