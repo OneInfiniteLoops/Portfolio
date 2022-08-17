29b24a28e4b8d5f6b15b21ec35b8308ebc46472d
1-Infiniteloop <skjyeung@gmail.com>
2022-08-17 15:16:11 +0100

    Edits intro content

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -5,11 +5,12 @@ const Introduction = () => {
     <div className="Introduction">
       <p>Hi, my name is</p>
       <h1>Siu Kwan James Yeung.</h1>
-      <h2>I'm a Full-Stack Software Developer. 🧑🏻‍💻</h2>
+      <h2>I'm a Software Developer. 🧑🏻‍💻</h2>
       <p>
-        A highly driven and curious software developer. I recently completed a
-        coding bootcamp at Northcoders in Manchester where I acquired experience
-        in Node.JS, React, PostgreSQL and more.
+        A software developer with a background in economics, finance, and
+        international political economy. I recently completed a coding bootcamp
+        at Northcoders in Manchester where I acquired experience in Node.JS,
+        React, PostgreSQL, and more.
       </p>
       <Profile />
     </div>
