a291946e4e843f7a39b3be8fdde5b38532e95ad9
1-Infiniteloop <skjyeung@gmail.com>
2022-08-19 21:08:29 +0100

    Changes id to className for work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -46,7 +46,7 @@ const Work = () => {
   ]);
 
   return (
-    <div id="work">
+    <div className="work">
       <h1>Check Out My Projects Here.</h1>
       <ul className="portfolio-list">
         {portfolioList.map((portfolioItem) => {
